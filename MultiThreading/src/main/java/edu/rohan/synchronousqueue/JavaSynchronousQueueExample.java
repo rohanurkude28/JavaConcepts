ce122c47223095487f343ffba94645db4fbe7d60
Rohan Urkude <rohan.urkude@here.com>
2022-03-21 23:57:24 +0530

    Add explanation of Executor service and implement Callable

---
+++ MultiThreading/src/main/java/edu/rohan/synchronousqueue/JavaSynchronousQueueExample.java
@@ -0,0 +1,18 @@
+package edu.rohan.synchronousqueue;
+
+import java.util.concurrent.SynchronousQueue;
+ 
+public class JavaSynchronousQueueExample {
+ 
+    public static void main(String args[]) {
+        final SynchronousQueue queue = new SynchronousQueue();
+         
+        // start publisher thread
+        new Thread(new QueueProducer(queue)).start();
+         
+        // start consumer thread
+        new Thread(new QueueConsumer(queue)).start();
+     
+    }
+ 
+}
